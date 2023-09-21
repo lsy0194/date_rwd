4f55e2d2825a011b19a597523e1b55c329f303c9
lsy0194 <tktmadltmddu@naver.com>
2023-09-21 09:13:57 +0900

    현재 시간값에 따라 am, pm붙이기

--- main.js
+++ main.js
@@ -1,6 +1,64 @@
-//외부 스크립트 파일을 head안쪽에서 defer방식으로 출력
-//main.js 해석과 DOM 랜더링을 병렬식으로 동시에 실행
-//해석은 동시에 하지만 실제 호출은 화면의 렌더링이 완료시 호출
-//type='Module'적용하면 위의 기능과 전역변수의 모듈화 처리
-const box = document.querySelector('article');
-console.log(box);
+/*
+객체를 생성하는 방법
+- 객체리터럴을 통한 생성
+- 생성자함수를 통한 생성(new 키워드를 이용해서 동일한 구조의 객체를 복사할때 주로 사용)
+- 생성자 함수를 통해서 생성된 객체 :인스턴스 
+- 인스턴스 객체에는 prototype이라는 공통의 저장공간이 포함되어 있어서 자주쓰는 함수를 등록후 자유롭게 호출가능
+*/
+
+const now = new Date();
+console.dir(now);
+//우리나라 시간대은 전세계표준시간대에서 9시간 더해진 시간 (그리니치 표준시간대 적용)
+
+const months = [
+	'January',
+	'Februart',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const year = now.getFullYear();
+const month = now.getMonth(); //순번을 반환
+const date = now.getDate();
+const day = now.getDay();
+const hours = 12; //now.getHours();
+const min = now.getMinutes();
+const sec = now.getSeconds();
+
+let newHr = 0;
+if (hours > 12) {
+	newHr = hours - 12;
+	//이안에 추가조건문 오후일때
+	if (newHr < 10) {
+		newHr = `0${newHr}AM`;
+	} else {
+		newHr = newHr + 'pm';
+	}
+} else {
+	newHr = hours;
+	//이안에 추가조건문 오전일때
+	if (newHr < 10) {
+		newHr = `0${newHr}AM`;
+	} else {
+		newHr = newHr + 'am';
+	}
+}
+
+console.log(newHr);
+
+// if (hours > 0 && hours <= 12) {
+// 	console.log(`AM ${hours}: ${min}`);
+// }
+
+// if (hours > 12 && hours <= 24) {
+// 	console.log(`PM ${hours - 12}: ${min}`);
+// }
