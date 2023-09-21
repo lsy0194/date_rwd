c4c0b84812370ba74a3ed3525b6f5963cd0ecca9
lsy0194 <tktmadltmddu@naver.com>
2023-09-21 10:17:01 +0900

    화살표함수로 변경

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
+// const [hr, m, s] = document.querySelectorAll('h1 span');
+
 const now = new Date();
 const hours = now.getHours();
 const min = now.getMinutes();
 const sec = now.getSeconds();
-console.log(setNumbers(hours));
 
+// hr.innerText = setHours(setHours(hours));
+// m.innerText = setNumbers(min);
+// s.innerText = setNumbers(sec);
 function setNumbers(num) {
-	/*
-	if (num < 10) num = '0' + num;	
-	*/
-	//조건식 ? true일때 실행할 구문 : false일때 실행할 구문
-	//삼항연산자 구문안에는 대입연산자가 들어올수 없기 때문에 괄호로 묶어서 표현식으로 변경
-	//특정값이 else 일때 굳이 실행할 코드가 없다면 &&연산자 활용
-	//num < 10 ? (result = '0' + num) : (result = num);
 	num < 10 && (num = '0' + num);
-
 	return num;
 }
 
-/*
+// function setHours(num) {
+// 	num > 12 && (num = num - 12);
+// 	return num;
+// }
+//const setNumbers = (num) => num < 10 && (num = '0' + num);
